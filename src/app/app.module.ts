29ed5d06ffd7f87713b41631ae1b14c3552a4f75
dsam04 <dsam93593@gmail.com>
2024-04-04 21:39:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { AppComponent } from './app.component';
 import { UserProfileComponent } from '../components/user-profile/user-profile.component';
 import { HomePageComponent } from '../components/home-page/home-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatChipsModule} from '@angular/material/chips'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatIconModule} from '@angular/material/icon'
+import { MatChipGrid } from '@angular/material/chips';
 @NgModule({
   declarations: [
   AppComponent,
@@ -20,10 +24,14 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     HttpClientModule,
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
--- src/components/home-page/home-page.component.ts
+++ src/components/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-
+import { MatChipInputEvent } from '@angular/material/chips';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -103,6 +105,31 @@ export class HomePageComponent implements OnInit {
     this.age = parseInt(value, 10);
   }
 
+  keywords = ['play',] ;
+  formControl = new FormControl(['angular']);
+
+  announcer = inject(LiveAnnouncer);
+
+  removeKeyword(keyword: string) {
+    const index = this.keywords.indexOf(keyword);
+    if (index >= 0) {
+      this.keywords.splice(index, 1);
+
+      this.announcer.announce(`removed ${keyword}`);
+    }
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our keyword
+    if (value) {
+      this.keywords.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
 
 }
 
