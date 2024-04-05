56e8033d0ca97790729bebc4832c28d54dca3399
dsam04 <dsam93593@gmail.com>
2024-04-05 10:03:29 +0530

    first commit

--- src/components/home-page/home-page.component.ts
+++ src/components/home-page/home-page.component.ts
@@ -105,7 +105,7 @@ export class HomePageComponent implements OnInit {
     this.age = parseInt(value, 10);
   }
 
-  keywords = ['play',] ;
+  keywords = ['cricket','football','hockey'] ;
   formControl = new FormControl(['angular']);
 
   announcer = inject(LiveAnnouncer);
